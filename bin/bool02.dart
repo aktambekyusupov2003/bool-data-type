cfa20c9c87e868520401f56abd0e006a8fcc27e9
aktam <aktam36911@gmail.com>
2022-12-14 12:58:49 +0500

    bool02.dart

--- bin/bool02.dart
+++ bin/bool02.dart
@@ -1,10 +1,7 @@
-/*
-    Create function func
-    Check the logic "The variable 'a' is equal 7"
-    Args:
-        a: int
-    Returns:
-        bool
-*/
+bool func(int a) {
+  return a == 7;
+}
 
-void main() {}
+void main() {
+  print(func(7));
+}
